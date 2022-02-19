c4c871c2f986decd8bf8ea43f43908aad8ea6ba9
Alexander_Borisov <borisovab@mail.ru>
2022-02-19 16:22:21 +0300

    something

---
+++ swiftbook.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+import libkern
+
+var greeting = "Hello, playground"
+
+
+//let a = 10
+//let b = 20
+//func calculate (a: Int, b: Int) -> Int {
+//    return a - b
+//}
+//var c = calculate(a: b, b: a)
+//print(calculate(a: a, b: c))
+//
+//print(calculate(a: 10, b: 2))
+
+//MARK: - 1
+let sum: Int = 10
+var weight: UInt = 20
+let high: Float = 30.456
+let radius: Double = 40.123456789
+let greetings: String = "Come in"
+
+weight = UInt.max
+weight = UInt.min
+
+
+
